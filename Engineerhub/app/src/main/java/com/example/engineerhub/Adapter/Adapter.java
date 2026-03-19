8e8f23a897e87e54afa81d2bf881218d193e7713
mx2240 <lovemicky10@gmail.com>
2026-03-19 12:22:47 +0000

    Adapter updated

--- Engineerhub/app/src/main/java/com/example/engineerhub/Adapter/Adapter.java
+++ Engineerhub/app/src/main/java/com/example/engineerhub/Adapter/Adapter.java
@@ -1,4 +1,10 @@
 package com.example.engineerhub.Adapter;
 
 public class Adapter {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+
+    }
 }
